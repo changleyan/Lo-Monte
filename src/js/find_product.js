ae8341c39982d9868ed3c7c5f6a9cde1cd97b120
Chang <changleyan1@gamail.com>
2022-02-20 15:09:11 +0100

    Initial Commit -Version 4 de la app ok

---
+++ src/js/find_product.js
@@ -0,0 +1,20 @@
+import { d, URLactual, setContinuar } from "./tools/constants.js";
+import { peticiones } from "./mount_product.js";
+
+const adicionarCarrito = async () => {
+  if (URLactual.toLocaleLowerCase().includes("Products".toLocaleLowerCase())) {
+    setContinuar(true);
+  } else {
+    setContinuar(false);
+  }
+  try {
+    const existBtnAdd = await d.getElementsByClassName("input-append");
+    if (existBtnAdd !== null && existBtnAdd.length > 0) {
+      peticiones();
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { adicionarCarrito };
